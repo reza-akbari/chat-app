ed00d0a3d2b14e9715b76e9efa2cf02edcfe0314
Reza Akbari <co.alphacodes@gmail.com>
2023-02-27 08:16:15 +0330

    fix local auth bug

--- next/utils/localAuth.ts
+++ next/utils/localAuth.ts
@@ -17,21 +17,31 @@ const parseToken = (token: string) => {
   return JSON.parse(atob(b64str));
 };
 
+let refreshPromise = Promise.resolve();
 const refreshAuthToken = async (): Promise<string | undefined> => {
-  try {
-    const { accessToken } = await fetch(API_BASE + "/auth/refresh", {
-      method: "POST",
-      credentials: "include",
-    }).then((resp) => resp.json());
-    setAuthToken(accessToken);
-    return accessToken;
-  } catch (err) {
-    console.error(err);
-  }
-  return undefined;
+  let resolve = () => {};
+  refreshPromise = new Promise((rs) => {
+    resolve = rs;
+  });
+  const token = await (async () => {
+    try {
+      const { accessToken } = await fetch(API_BASE + "/auth/refresh", {
+        method: "POST",
+        credentials: "include",
+      }).then((resp) => resp.json());
+      setAuthToken(accessToken);
+      return accessToken;
+    } catch (err) {
+      console.error(err);
+    }
+    return undefined;
+  })();
+  resolve();
+  return token;
 };
 
 export const getAuthToken = async (): Promise<string | undefined> => {
+  await refreshPromise;
   if (inMemoryToken) {
     const { exp } = parseToken(inMemoryToken);
     if (exp < Date.now() / 1000) {
